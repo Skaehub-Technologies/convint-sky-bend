d7cdc010de30332847ca9e8aab437acd34f5a6d2
nyaribari <anyaribari@gmail.com>
2022-06-07 14:03:35 +0300

    ch-add-noqa-formatter

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -1,10 +1,10 @@
 import os
 
-from .base import *
+from .base import *  # noqa
 
 if os.getenv("ENV_NAME") == "Production":
-    from .production import *
+    from .production import *  # noqa
 elif os.getenv("ENV_NAME") == "Staging":
-    from .staging import *
+    from .staging import *  # noqa
 else:
-    from .local import *
+    from .local import *  # noqa
