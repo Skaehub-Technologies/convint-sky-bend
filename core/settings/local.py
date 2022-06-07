64ff625e39e23b85bdd97f08d611213dd5ff24de
Nyaribari Reuben <39657169+foscraft@users.noreply.github.com>
2022-06-07 12:03:49 +0300

    Update local.py

--- core/settings/local.py
+++ core/settings/local.py
@@ -1,29 +1,10 @@
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "localhost",
+    "localhost","127.0.0.1",
 ]
 
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-# Password validation
-# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
-]
