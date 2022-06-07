35b9e01fdef5c6e7a3cff1ce50eca6082ac149b9
Nyaribari Reuben <39657169+foscraft@users.noreply.github.com>
2022-06-07 12:02:22 +0300

    Update base.py

--- core/settings/base.py
+++ core/settings/base.py
@@ -55,6 +55,17 @@ DATABASES = {
 }
 
 
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+
 
 LANGUAGE_CODE = "en-us"
 
