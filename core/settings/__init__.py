20658deb7f56538cb12f6e1347aefb7de5c6899e
nyaribari <anyaribari@gmail.com>
2022-06-07 14:42:56 +0300

    ch-init seetings

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -1,10 +1,10 @@
 import os
 
-from .base import *  # noqa
+from .base import *  # noqa: F403, F401
 
 if os.getenv("ENV_NAME") == "Production":
-    from .production import *  # noqa
+    from .production import *  # noqa: F403, F401
 elif os.getenv("ENV_NAME") == "Staging":
-    from .staging import *  # noqa
+    from .staging import *  # noqa: F403, F401
 else:
-    from .local import *  # noqa
+    from .local import *  # noqa: F403, F401
