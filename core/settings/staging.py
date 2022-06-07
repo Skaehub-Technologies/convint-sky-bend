c2b60f03436b9e2b7a34b2e55938cfb759bc5fdb
Nyaribari Reuben <39657169+foscraft@users.noreply.github.com>
2022-06-07 12:08:22 +0300

    Update staging.py

--- core/settings/staging.py
+++ core/settings/staging.py
@@ -1,22 +1,8 @@
-DEBUG = True
+DEBUG = False
 
+#e.g heroku host
 ALLOWED_HOSTS = [""]
 
 
 
 
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
-]
