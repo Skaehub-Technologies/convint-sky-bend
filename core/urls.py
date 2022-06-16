afc64b437771674c8759af3e1fe0269b11a62eef
Claire Mwaniki <neemaclare@gmail.com>
2022-06-16 15:03:51 +0300

    Modified pipfile

--- core/urls.py
+++ core/urls.py
@@ -17,12 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
+import os
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/auth/", include("users.urls")),
 ]
-
-if os.getenv("ENV_NAME") == "local":
-        urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
-                              
