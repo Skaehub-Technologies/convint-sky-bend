ec4625a9764549632c19247f287c8e7fee80806e
narravabrion <kidbrion7@gmail.com>
2022-06-19 12:03:58 +0300

    ft(reset-password-via-email): enable users to reset passwords via email

--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-
-import os
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/auth/", include("users.urls")),
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from .views import UserTokenObtainPairView
+from .views import (
+    PasswordResetAPIView,
+    PasswordResetEmail,
+    UserTokenObtainPairView,
+)
 
 urlpatterns = [
     path(
@@ -10,4 +14,19 @@ urlpatterns = [
         name="login",
     ),
     path("refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path(
+        "reset-password-request/",
+        PasswordResetEmail.as_view(),
+        name="reset-password-request",
+    ),
+    path(
+        "reset-password/<uidb64>/<token>",
+        PasswordResetAPIView.as_view(),
+        name="reset-password-verify",
+    ),
+    path(
+        "reset-password/",
+        PasswordResetAPIView.as_view(),
+        name="reset-password",
+    ),
 ]
