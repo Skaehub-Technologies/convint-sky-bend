7342838973c5bee28e0407bbad7cd1ae822275c7
Claire Mwaniki <neemaclare@gmail.com>
2022-06-21 12:30:01 +0300

    resolving coflicts

--- users/test/test_models.py
+++ users/test/test_models.py
@@ -1,15 +1,10 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-<<<<<<< HEAD
-from django.test import Client
-=======
->>>>>>> 04d5b395ca31431397f3de7cc1300e9d85555349
 from faker import Faker
 
 fake = Faker()
 User = get_user_model()
 
-<<<<<<< HEAD
 class UserModelTest(TestCase):
     def test_create_user(self):
         user = User.objects.create_user(
@@ -71,51 +66,9 @@ class UserModelTest(TestCase):
         password=fake.password(),
         is_superuser=True,)
         self.assertTrue = (user.is_superuser)
-      
-=======
-
-class TestUserModel(TestCase):
-    def test_create_user(self) -> None:
-        email = fake.email()
-        user = User.objects.create_user(email=email, password=fake.password())
-        self.assertEqual(user.email, email)
-
-    def test_password_exception_raised(self) -> None:
-        with self.assertRaises(ValueError):
-            User.objects.create_user(email=fake.email(), password="")
-
-    def test_Email_exception_raised(self) -> None:
-        with self.assertRaises(ValueError):
-            User.objects.create_user(email="", password=fake.password())
-
-    def test_is_editor(self) -> None:
-        email = fake.email()
-        user = User.objects.create_editor(
-            email=email, password=fake.password()
-        )
-        self.assertEqual(user.email, email)
-        self.assertTrue(user.is_editor)
-
-    def test_superuser(self) -> None:
-        email = fake.email()
-        user = User.objects.create_superuser(
-            email=email, password=fake.password()
-        )
-        self.assertEqual(user.email, email)
-        self.assertTrue(user.is_superuser)
-        self.assertTrue(user.is_staff)
-
-    def test_superuser_password_error(self) -> None:
-        with self.assertRaises(ValueError):
-            User.objects.create_superuser(email=fake.email(), password="")
-
-    def test_superuser_email_error(self) -> None:
-        with self.assertRaises(ValueError):
-            User.objects.create_superuser(email="", password=fake.password())
-
+        
     def test_user_representation_is_email(self) -> None:
         user = User.objects.create_user(
             email=fake.email(), password=fake.password()
         )
-        self.assertEqual(str(user), user.email)
->>>>>>> 04d5b395ca31431397f3de7cc1300e9d85555349
+        self.assertEqual(str(user), user.email)  
