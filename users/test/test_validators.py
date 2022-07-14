a241d07e83191591d26e624d214840205f2ad4e7
nyaribari <anyaribari@gmail.com>
2022-07-14 09:01:02 +0300

    ch(validators-tests)

--- users/test/test_validators.py
+++ users/test/test_validators.py
@@ -1,4 +1,5 @@
 import pytest
+from django.core.exceptions import ValidationError
 
 from users.validators import (
     validate_password_digit,
@@ -11,15 +12,23 @@ from users.validators import (
 @pytest.mark.parametrize("value", ["123456789", "1234567890"])
 def test_validate_password_digit(value: str) -> None:
     assert validate_password_digit(value) == value
+    with pytest.raises(ValidationError):
+        validate_password_digit("TYU@wdhdh")
 
 
 def test_validate_password_uppercase() -> None:
-    assert validate_password_uppercase("123456789") == "123456789"
+    assert validate_password_uppercase("T123456789") == "T123456789"
+    with pytest.raises(ValidationError):
+        validate_password_uppercase("1234567890")
 
 
 def test_validate_password_symbol() -> None:
-    assert validate_password_symbol("123456789") == "123456789"
+    assert validate_password_symbol("@123456789") == "@123456789"
+    with pytest.raises(ValidationError):
+        validate_password_symbol("1234567890")
 
 
 def test_validate_password_lowercase() -> None:
-    assert validate_password_lowercase("123456789") == "123456789"
+    assert validate_password_lowercase("t123456789") == "t123456789"
+    with pytest.raises(ValidationError):
+        validate_password_lowercase("1234567890")
